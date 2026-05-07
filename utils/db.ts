7ab71c292f4476f793fcdd5a21bf7b6e77819778
jaws <dis.131998@gmail.com>
2026-05-07 15:12:55 +0300

    add: database util

---
+++ utils/db.ts
@@ -0,0 +1,26 @@
+import config from "./config.ts";
+import logger from "./logger.ts";
+import mongoose from "mongoose";
+
+const connect = async () => {
+    try {
+        logger.info('connecting to', config.MONGODB_URI as string)
+        await mongoose.connect(config.MONGODB_URI as string, { family: 4 })
+        logger.info('connected to MongoDB');
+    } catch(e) {
+        if (e instanceof Error) logger.error('error connection to MongoDB:', e.message)
+    }
+   
+}
+const disconnect = async () => {
+    await mongoose.disconnect();
+    logger.info("disconected from MongoDB")
+}
+const drop = async () => {
+    if (mongoose.connection.db) {
+        await mongoose.connection.db.dropDatabase();
+    }
+   
+}
+
+export default { connect, disconnect, drop }
