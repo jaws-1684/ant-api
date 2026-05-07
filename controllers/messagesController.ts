dead5b68146184c68676186dc96127c7b6c6862a
jaws <dis.131998@gmail.com>
2026-05-07 15:16:22 +0300

    add: CRUD actions to messagesController

---
+++ controllers/messagesController.ts
@@ -0,0 +1,62 @@
+import messageService from '../services/messageService.ts';
+import type { MessageDTO, MessagePayload } from "../types/index.ts";
+import type { Response, Request, NextFunction } from 'express';
+import { getCurrentUserId } from '../controllers/auth.ts';
+import { parseId, parseMessage, parseUpdateMessage } from '../utils/parsers.ts';
+
+
+const createMessage = async (
+  request: Request<unknown, unknown, MessagePayload>, 
+  response: Response<MessageDTO>, 
+  next: NextFunction
+) => {
+  try {
+    const currentUserId = getCurrentUserId(request);
+    const messagePayload = parseMessage(request.body)
+   
+    const message = {
+      ...messagePayload,
+      user: currentUserId,
+    };
+    const newMessage = await messageService.addMessage(message);
+    response.status(201).send(newMessage);
+  } catch(e: unknown) {
+    next(e);
+  }
+}
+
+const deleteMessage = async (
+  request: Request, 
+  response: Response<MessageDTO>, 
+  next: NextFunction
+) => {
+  try {
+    const currentUserId = getCurrentUserId(request);
+    const messageId = parseId(request.params.id);
+    const deletedMessage = await messageService.deleteMessage({ id: messageId, userId: currentUserId });
+    response.status(200).send(deletedMessage);
+  } catch(e: unknown) {
+    next(e);
+  }
+}
+const updateMessage = async (
+  request: Request, 
+  response: Response<MessageDTO>, 
+  next: NextFunction
+) => {
+    try {
+    const currentUserId = getCurrentUserId(request);
+    const messagePayload = parseUpdateMessage(request.body)
+   
+    const message = {
+      ...messagePayload,
+      user: currentUserId,
+    };
+    const updatedMessage = await messageService.updateMessage(message);
+    response.status(200).send(updatedMessage);
+  } catch(e: unknown) {
+    next(e);
+  }
+}
+
+export default { createMessage, deleteMessage, updateMessage }
