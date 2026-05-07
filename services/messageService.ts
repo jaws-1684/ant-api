0fae3084b1fc8e51bd91942b6995c23d259faa16
jaws <dis.131998@gmail.com>
2026-05-07 13:00:43 +0300

    add messageService

--- services/messageService.ts
+++ services/messageService.ts
@@ -1,13 +1,38 @@
-import type { MessageEntry } from "../types.ts";
-const getMessages = (): MessageEntry[] => {
-    return [{
-        id: 1,
-        content: "hello",
-        creatorId: 1,
-        createdAt:  new Date().toUTCString(),
-    }];
-};
+import type { NewMessage, MessageDTO, UpdateMessage } from "../types/index.ts";
+import Message from "../models/message.ts";
+import Chat from "../models/chat.ts";
 
+
+const getMessages = async ({ chatId, userId }: { chatId: string, userId: string }): Promise<MessageDTO[]> => {
+    const chat = await Chat.findOne({ _id: chatId, participants: userId }).orFail();
+    return Message.find({ chat: chat._id });
+};
+const addMessage = async (newMessage: NewMessage): Promise<MessageDTO> => {
+    await Chat.findOne({ _id: newMessage.chat, participants: newMessage.user }).orFail();
+    return new Message(newMessage).save();
+};
+const updateMessage = async (updateMessage: UpdateMessage): Promise<MessageDTO> => {
+    const message = await Message.findOne({ _id: updateMessage.id, user: updateMessage.user }).orFail();
+    message.content = updateMessage.content;
+    return await message.save()
+};
+const deleteMessage = async ({ id, userId }: { id: string, userId: string }) => {
+    await Message.deleteOne({ _id: id, user: userId }).orFail();
+}
+const findMessage = async ({ id, userId }: { id: string, userId: string }) => {
+    return Message.findOne({ _id: id, user: userId });
+}
+const insertMessages = async (messages: NewMessage[]) => {
+    return await Message.insertMany(messages);
+}
+
+const dropMessages = async () => Message.deleteMany({});
 export default {
-    getMessages
+    getMessages,
+    addMessage,
+    dropMessages,
+    deleteMessage,
+    findMessage,
+    updateMessage,
+    insertMessages
 };
