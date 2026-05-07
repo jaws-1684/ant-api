a8465345d0c8ed60c99614771bce132d98e8154b
jaws <dis.131998@gmail.com>
2026-05-07 13:01:36 +0300

    add messageModel

---
+++ models/messageModel.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+import type { MessageDTO, MessageDocument } from "../types/index.ts";
+
+export const messageSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    user: {
+        type: Schema.Types.ObjectId, ref: 'User',
+        required: true
+    },
+    chat: {
+        type: Schema.Types.ObjectId, ref: 'Chat',
+        required: true
+    },
+   
+}, { timestamps: true});
+
+messageSchema.set('toJSON', {
+  transform: (_document: MessageDocument, returnedObject: MessageDTO) => {
+    returnedObject.id = returnedObject._id?.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+})
+
+const Message = mongoose.model<MessageDocument>('Message', messageSchema);
+
+
+export default Message;
+
