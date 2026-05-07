0a8f108b98c24d3ee3c0b9dd9fda2db3385a9a59
jaws <dis.131998@gmail.com>
2026-05-07 15:13:11 +0300

    add: node ENV config

--- utils/config.ts
+++ utils/config.ts
@@ -4,5 +4,13 @@ dotenv.config({ path: '.env' });
 const MONGODB_URI = process.env.NODE_ENV == "test" ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
 export default {
     PORT: process.env.PORT || 3001,
-    MONGODB_URI
+    MONGODB_URI,
+    BASE_URL: process.env.BASE_URL,
+    JWT_SECRET          : process.env.JWT_SECRET,
+    JWT_REFRESH_SECRET  : process.env.JWT_REFRESH_SECRET,
+    GOOGLE_CLIENT_ID    : process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GITHUB_CLIENT_ID    : process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    CLIENT_URL          : process.env.CLIENT_URL || 'http://localhost:5173'
 };
