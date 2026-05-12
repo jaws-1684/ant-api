ad00ee99ef45163d5c02ccd337d0c90c03c093aa
jaws <dis.131998@gmail.com>
2026-05-12 17:58:42 +0300

    add: number and int parsers

--- utils/w.ts
+++ utils/w.ts
@@ -67,6 +67,12 @@ export function url() {
 export function objectId() {
   return new WObjectId()
 }
+export function number() {
+  return new WNumber()
+}
+export function int() {
+  return new WInt()
+}
 // -------------------------------------------------------------------------------------
 // Classes
 // -------------------------------------------------------------------------------------
@@ -317,3 +323,77 @@ class WPassword extends WString {
   }
 }
 
+class WNumber extends WType<number> {
+  protected validate(ctx: ParseContext) {
+    if (typeof ctx.value === "number" && Number.isFinite(ctx.value)) {
+      return ctx
+    }
+
+    ctx.issues.push({ path: [], message: "Expected number", fatal: true })
+
+    return ctx
+  }
+
+  min(n: number) {
+    this.validators.push(ctx => {
+      if (ctx.value >= n) return true
+
+      ctx.issues.push({ path: [], message: "Number is too small" })
+
+      return false
+    })
+
+    return this
+  }
+
+  max(n: number) {
+    this.validators.push(ctx => {
+      if (ctx.value <= n) return true
+
+      ctx.issues.push({ path: [], message: "Number is too large" })
+
+      return false
+    })
+
+    return this
+  }
+
+  positive() {
+    this.validators.push(ctx => {
+      if (ctx.value > 0) return true
+
+      ctx.issues.push({ path: [], message: "Number must be positive" })
+
+      return false
+    })
+
+    return this
+  }
+
+  negative() {
+    this.validators.push(ctx => {
+      if (ctx.value < 0) return true
+
+      ctx.issues.push({ path: [], message: "Number must be negative" })
+
+      return false
+    })
+
+    return this
+  }
+}
+
+class WInt extends WNumber {
+  protected validate(ctx: ParseContext) {
+    const newCtx = super.validate(ctx)
+    if (ctx.issues.length !== newCtx.issues.length) return newCtx
+
+    const numberCtx = newCtx as ParseContext<number>
+
+    if (Number.isInteger(numberCtx.value)) return numberCtx
+
+    numberCtx.issues.push({ path: [], message: "Expected integer" })
+
+    return numberCtx
+  }
+}
