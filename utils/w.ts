abc7aa74ec74cdef85b8649fd1eab2f3a6e3b4ab
jaws <dis.131998@gmail.com>
2026-05-15 16:00:20 +0300

    add: WArray type

--- utils/w.ts
+++ utils/w.ts
@@ -94,6 +94,9 @@ export function object<T extends Record<PropertyKey, WType<any>>>(
 export function string() {
   return new WString()
 }
+export function array<T>(schema: WType<T>) {
+  return new WArray(schema)
+}
 export function email() {
   return new WEmail()
 }
@@ -334,6 +337,76 @@ class WObject<
   }
   get schema () {
     return this.#schema
+  }
+}
+class WArray<T> extends WType<T[]> {
+  #inner: WType<T>
+
+  constructor(schema: WType<T>) {
+    super()
+    this.#inner = schema
+  }
+
+  protected validate(ctx: ParseContext) {
+    if (!Array.isArray(ctx.value)) {
+      ctx.issues.push({ message: "Expected array", path: [], fatal: true })
+      return ctx
+    }
+
+    for (let i = 0; i < ctx.value.length; i++) {
+      const item = ctx.value[i]
+      const innerRes = this.#inner.safeParse(item)
+      if (isOk(innerRes)) {
+        ctx.value[i] = innerRes.data
+      } else {
+        ctx.issues.push(
+          ...innerRes.error.issues.map(issue => {
+            return {
+              ...issue,
+              path: [i, ...issue.path],
+            }
+          })
+        )
+      }
+    }
+
+    return ctx
+  }
+
+  min(length: number) {
+    this.validators.push(ctx => {
+      if (ctx.value.length >= length) return true
+
+      ctx.issues.push({ path: [], message: "Array is too short" })
+
+      return false
+    })
+
+    return this
+  }
+
+  max(length: number) {
+    this.validators.push(ctx => {
+      if (ctx.value.length <= length) return true
+
+      ctx.issues.push({ path: [], message: "Array is too long" })
+
+      return false
+    })
+
+    return this
+  }
+
+  length(length: number) {
+    this.validators.push(ctx => {
+      if (ctx.value.length === length) return true
+
+      ctx.issues.push({ path: [], message: "Array is not the exact length" })
+
+      return false
+    })
+
+    return this
   }
 }
 class WString extends WType<string> {
