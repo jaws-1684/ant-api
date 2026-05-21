b7d092e3bd431be60249e34aa10428d880244e0b
jaws <dis.131998@gmail.com>
2026-05-21 16:14:44 +0300

    implement: message and chat serializer

--- utils/serializers.ts
+++ utils/serializers.ts
@@ -1,18 +1,49 @@
 import type { mongo } from "mongoose";
-import type { UserDocument, UserDTO } from "../types/index.ts";
+import type { ChatDocument, ChatDTO, MessageDocument, MessageDTO, UserDocument, UserDTO } from "../types/index.ts";
 
 const pick = <T extends mongo.Document, K extends keyof T>(obj: T, keys: K[]) => {
     return Object.fromEntries(
         Object.entries(obj).filter(([key]) => keys.includes(key as K))
     ) as { [P in K]: T[P] };
 };
+const omit = <T extends mongo.Document, K extends keyof T>(obj: T, keys: K[]) => {
+    return Object.fromEntries(
+        Object.entries(obj).filter(([key]) => !keys.includes(key as K))
+    ) as { [P in keyof T]: T[P] };
+};
+
 export const userSerializer = (userDocument: UserDocument): UserDTO => {
-    return {...pick(userDocument, [
+    const serialized = pick(userDocument, [
         "email", 
         "username", 
         "image", 
         "chats", 
-        "deleted"]),
-        id: userDocument?._id?.toString()
+    ]);
+    return {
+        ...serialized,    
+        id: userDocument?._id?.toString() ?? userDocument?.id,
     };
 };
+export const chatSerializer = (chatDocument: ChatDocument): ChatDTO => {
+    const serialized = omit(chatDocument, [
+        "_id",
+        "__v",
+        "participants"
+    ]);
+    const participants = chatDocument.participants.map((u: mongo.Document) => userSerializer(u as UserDocument));
+    return {
+        ...serialized,
+        participants,
+        id: chatDocument?.id?.toString()
+    };
+};
+export const messageSerializer = (messageDocument: MessageDocument): MessageDTO => {
+    const serialized = omit(messageDocument, [
+        "_id",
+        "__v",
+    ]);
+    return {
+        ...serialized,
+        id: messageDocument?.id?.toString()
+    };
+}
