f864a991a2f156bfbd720f731fffbbce70727c04
jaws <dis.131998@gmail.com>
2026-05-21 11:43:16 +0300

    add: [] to the request logger

--- utils/middleware.ts
+++ utils/middleware.ts
@@ -1,35 +1,50 @@
 import type { Request, Response, NextFunction } from "express";
 import logger from "../utils/logger.ts";
+import passport from '../utils/passport.ts'
+import { UnauthorizedError, toAppError } from "./errors.ts";
 
 const requestLogger = (request: Request, _: Response, next: NextFunction) => {
-    logger.info('Method:', request.method);
-    logger.info('Path:  ', request.path);
-    logger.info('Body:  ', JSON.stringify(request.body));
+    logger.info('[Method]:', request.method);
+    logger.info('[Path]:  ', request.path);
+    logger.info('[Body]:  ', JSON.stringify(request.body));
     logger.info('---');
-    next();
+    next(); 
 };
 
 const unknownEndpoint = (_request: Request, response: Response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
 
-const errorHandler = (error: Error, _request: Request, response: Response, next: NextFunction) => {
-  logger.error(error.message);
+const errorHandler = (error: unknown, _request: Request, response: Response, next: NextFunction) => {
+    if (error && error instanceof Error) {
+        logger.error(error.message);
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
-  }
-
-  next(error);
-  return;
+        const appError = toAppError(error);
+        
+        if (appError) {
+            const { error, status } = appError.toObject()
+            return response.status(status).json({ error })
+        }
+    };
+    return next(error);
 };
+
+
+const authMiddleware = (request: Request, response: Response, next: NextFunction) => {
+    passport.authenticate('jwt', { session: false }, (err: Error, user: Express.User) => {
+        if (err || !user) {
+            return next(new UnauthorizedError())
+        }
+        request.user = user
+        return next()
+    })(request, response, next)
+}
 
 export default {
     requestLogger,
     unknownEndpoint,
-    errorHandler
+    errorHandler,
+    authMiddleware,
 };
 
 
