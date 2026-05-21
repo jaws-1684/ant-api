09fbcb03fee599198b880d3f56b0f90012c6f393
jaws <dis.131998@gmail.com>
2026-05-21 11:36:58 +0300

    add: data entries

---
+++ data/userEntries.ts
@@ -0,0 +1,23 @@
+export default [
+  { "username": "oliver92", "email": "oliver.smith@gmail.com", "password": "password123" },
+  { "username": "geo_dev", "email": "george.brown@outlook.com", "password": "password123" },
+  { "username": "harry.codes", "email": "harry.wilson@yahoo.com", "password": "password123" },
+  { "username": "jack_01", "email": "jack.taylor@gmail.com", "password": "password123" },
+  { "username": "noahx", "email": "noah.anderson@proton.me", "password": "password123" },
+  { "username": "charlie.dev", "email": "charlie.thomas@gmail.com", "password": "password123" },
+  { "username": "tommy99", "email": "thomas.jackson@outlook.com", "password": "password123" },
+  { "username": "oscar_live", "email": "oscar.white@yahoo.com", "password": "password123" },
+  { "username": "james.codes", "email": "james.harris@gmail.com", "password": "password123" },
+  { "username": "will_tech", "email": "william.martin@icloud.com", "password": "password123" },
+
+  { "username": "olivia_x", "email": "olivia.thompson@gmail.com", "password": "password123" },
+  { "username": "emma.dev", "email": "emma.garcia@outlook.com", "password": "password123" },
+  { "username": "ava.codes", "email": "ava.martinez@yahoo.com", "password": "password123" },
+  { "username": "sophia01", "email": "sophia.robinson@gmail.com", "password": "password123" },
+  { "username": "bella_live", "email": "isabella.clark@proton.me", "password": "password123" },
+  { "username": "mia_tech", "email": "mia.rodriguez@gmail.com", "password": "password123" },
+  { "username": "amelia.dev", "email": "amelia.lewis@icloud.com", "password": "password123" },
+  { "username": "harper99", "email": "harper.lee@yahoo.com", "password": "password123" },
+  { "username": "eve.codes", "email": "evelyn.walker@gmail.com", "password": "password123" },
+  { "username": "abby01", "email": "abigail.hall@outlook.com", "password": "password123" }
+];
