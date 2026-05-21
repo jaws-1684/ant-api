1278b1fedc68aba4d5431cc427c45dc0cb8d7f9f
jaws <dis.131998@gmail.com>
2026-05-21 11:56:08 +0300

    add: chatRouter

---
+++ routes/chatRouter.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import chatsController from '../controllers/chatsController.ts';
+const router = express.Router();
+
+router.get('/', chatsController.getChats);
+router.get('/:id', chatsController.getMessages);
+router.post('/', chatsController.createChat);
+router.delete('/:id', chatsController.deleteChat);
+export default router;
