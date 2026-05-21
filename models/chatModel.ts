c0078021bc96331fc5962582e8b1888703851280
jaws <dis.131998@gmail.com>
2026-05-21 11:57:59 +0300

    add: chat group fields and safe accessor to participants transform

--- models/chatModel.ts
+++ models/chatModel.ts
@@ -8,14 +8,21 @@ export const chatSchema = new Schema({
         type: Map,
         of: Date,
         default: {}
-    }
+    },
+    name: {
+      type: String,
+      default: null
+    },
+    isGroup: { type: Boolean, default: false },
+    admin: { type: String },
+    closed: { type: Boolean, default: false }
 });
 
 chatSchema.set('toJSON', {
   transform: (_document: ChatDocument, returnedObject: Record<string, any>) => {
     returnedObject.id = returnedObject._id?.toString();
     returnedObject.participants = returnedObject.participants.map((p: UserDocument) => ({
-      id: p.id,
+      id: p._id?.toString(),
       username: p.username,
       email: p.email,
       image: p.image ?? "https://i.pravatar.cc/300"
