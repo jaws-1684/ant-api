c25c8f7371f0176671265284d0608fd972c62254
jaws <dis.131998@gmail.com>
2026-05-21 12:58:13 +0300

    add: patch route

--- routes/chatRouter.ts
+++ routes/chatRouter.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 router.get('/', chatsController.getChats);
 router.get('/:id', chatsController.getMessages);
 router.post('/', chatsController.createChat);
+router.patch('/:id', chatsController.updateChat);
 router.delete('/:id', chatsController.deleteChat);
 export default router;
