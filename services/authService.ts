b2c9666efb4341ca126ff034ddc34891f71f8872
jaws <dis.131998@gmail.com>
2026-05-21 11:54:00 +0300

    implement: #updateCredentials and #deleteProfile methods and move authorization logic to utils/auth.ts

--- services/authService.ts
+++ services/authService.ts
@@ -1,10 +1,11 @@
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
 import config from '../utils/config.ts';
 import userService from './userService.ts';
 import { ForbiddenError, InvalidCredentialsError } from '../utils/errors.ts';
 import { generateAccessToken, generateRefreshToken } from '../utils/tokens.ts';
-import type { LoginPayload, UserDTO } from '../types/index.ts';
+import type { LoginPayload, UpdateCredentialsPayload, UserDocument } from '../types/index.ts';
+import { authorizedUser, hashPassword, matchPassword } from '../utils/auth.ts';
+
 
 const createAccessToken = async (refreshToken: string): Promise<string> => {
     const decoded = jwt.verify(refreshToken, config.JWT_REFRESH_SECRET as string) as { userId: string };
@@ -17,22 +18,38 @@ const createRefreshToken = async(userId: string): Promise<string> => {
     await userService.updateRefreshToken({ id: userId, refreshToken });
     return refreshToken
 }
-const loginUser = async (loginUser: LoginPayload): Promise<UserDTO> => {
+const loginUser = async (loginUser: LoginPayload): Promise<UserDocument> => {
     const user = loginUser.email
         ? await userService.findByEmail(loginUser.email)
         : await userService.findByUsername(loginUser.username!)
     const password = loginUser.password       
-    if (!user) {
+    if (!user || user.deleted) {
         const dummyHash = '$2b$10$invalidhashtopreventtimingattacks000000000000000000000';
-        await bcrypt.compare(password, dummyHash);
-        throw new InvalidCredentialsError();
+        const match = await matchPassword(password, dummyHash) as { ok: false, error: InvalidCredentialsError }
+        throw match.error
     }  
-    const match = await bcrypt.compare(password, user.password!);
-    if (!match)  throw new InvalidCredentialsError();
-    return { ...user.toJSON(), id: user.id }
+    return authorizedUser({ id: user.id, password })
 }
+
+
+const updateCredentials = async (updateUserCredentials: UpdateCredentialsPayload): Promise<UserDocument | null> => {
+    const { id, email, password, newPassword } = updateUserCredentials
+    const user = await authorizedUser({ id, password })
+
+    if (email !== undefined) user.email = email;
+    if (newPassword !== undefined) user.password = await hashPassword(newPassword);
+
+    return user.save()
+};
+const deleteProfile = async (deleteUser: Pick<UpdateCredentialsPayload, "id" | "password">): Promise<void> => {
+    const user = await authorizedUser(deleteUser)
+    user.deleted = true,
+    await user.save()
+};
 export default {
     createAccessToken,
     createRefreshToken,
-    loginUser
+    loginUser,
+    updateCredentials,
+    deleteProfile
 }
