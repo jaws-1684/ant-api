93f6686c543d965d6262225d8daf42a8187a0118
jaws <dis.131998@gmail.com>
2026-05-21 11:44:23 +0300

    refactor: schemas with the new w.union() type

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -1,27 +1,46 @@
+
 import * as w from "./w.ts"
 
+export const pageSchema = w.coerce.number().int().min(1).default(1)
+export const passwordSchema =  w.password().min(8).max(20)
+export const objectIdSchema = w.objectId()
+export const idSchema = w.object({
+    id: w.objectId()
+})
 export const userSchema = w.object({
-    username: w.string().min(3),
+    username: w.string().min(3).max(20).escape(),
     email: w.email(),
-    password: w.password().min(8),
-    image: w.url().optional()
+    password: passwordSchema,
+    image: w.url().image().nullable().optional()
 })
-export const updateUserSchema = w.object({
-    id: w.objectId(),
-    username: w.string().min(3).optional(),
-    email: w.email().optional(),
-    password: w.password().min(8).optional(),
-    image: w.url().optional()
-})
+export const updateUserSchema = w.union([
+    userSchema.pick(["username", "image"]).partial(),
+    idSchema,
+])
+export const updateCredentialsSchema = w.union([
+     userSchema.pick(["email", "password"]).partial({
+        password: false
+     }),
+     idSchema,
+     w.object({
+        newPassword: passwordSchema.optional()
+     })
 
+])
 export const messageSchema = w.object({
     chatId: w.objectId(),
     userId: w.objectId(),
-    content: w.string().min(1).max(5000)
+    images: w.array(w.url().image()).optional(),
+    content: w.string().trim().min(1).max(5000).escape()
 })
-export const updateMessageSchema = w.object({
-    id: w.objectId(),
-    userId: w.objectId(),
-    content: w.string().min(1).max(5000)
-})
-export const objectIdSchema = w.objectId()
+export const updateMessageSchema = w.union([
+    messageSchema.pick(["userId", "content"]),
+    idSchema
+])
+
+export const loginSchema = w.union([
+    userSchema.pick(["username", "email", "password"]).partial({
+        password: false
+    }),
+
+]).refine(obj => Boolean(obj.email) || Boolean(obj.username), "Email or username expected")
