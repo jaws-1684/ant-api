f21a7a12808feccd03af05e076132a772abcb80a
jaws <dis.131998@gmail.com>
2026-05-22 14:35:44 +0300

    add: searchController

---
+++ controllers/searchController.ts
@@ -0,0 +1,26 @@
+import type { Request, Response, NextFunction } from "express";
+import userService from "../services/userService.ts";
+import groupService from "../services/groupService.ts";
+import { stringSchema } from "../utils/schemas.ts";
+
+const searchUsers = async (request: Request, response: Response, next: NextFunction) => {
+  try {
+    const query = stringSchema.parse(request?.query);
+    const users = await userService.searchUsers(query);
+    response.send(users);
+  } catch (e) {
+    next(e);
+  }
+};
+
+const searchGroups = async (request: Request, response: Response, next: NextFunction) => {
+  try {
+    const query = stringSchema.parse(request?.query);
+    const groups = await groupService.searchGroups(query);
+    response.send(groups);
+  } catch (e) {
+    next(e);
+  }
+};
+
+export default { searchUsers, searchGroups };
