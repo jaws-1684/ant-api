e421c0c6727b55bcf02572d67650ece3e1f3af2e
jaws <dis.131998@gmail.com>
2026-05-22 12:53:16 +0300

    remove: unexpected async

--- utils/tokens.ts
+++ utils/tokens.ts
@@ -1,12 +1,12 @@
 import jwt from "jsonwebtoken";
 import config from "./config.ts";
 
-export const generateAccessToken = async (userId: string) => {
+export const generateAccessToken = (userId: string) => {
   return jwt.sign({ userId }, config.JWT_SECRET as string, {
     expiresIn: "15m",
   });
 };
-export const generateRefreshToken = async (userId: string) => {
+export const generateRefreshToken = (userId: string) => {
   return jwt.sign({ userId }, config.JWT_REFRESH_SECRET as string, {
     expiresIn: "7d",
   });
