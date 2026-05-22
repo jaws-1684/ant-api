57e987c98290ddefe169f9d1dcfce9c959918836
jaws <dis.131998@gmail.com>
2026-05-22 17:58:30 +0300

    add: redis url and refactor/assert process.env types

--- utils/config.ts
+++ utils/config.ts
@@ -1,30 +1,35 @@
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
+const env = process.env as Record<PropertyKey, string>;
 const MONGODB_URI =
-  process.env.NODE_ENV == "test"
-    ? process.env.TEST_MONGODB_URI
-    : process.env.MONGODB_URI;
+  env.NODE_ENV == "test"
+    ? env.TEST_MONGODB_URI
+    : env.MONGODB_URI;
+const REDIS_URL = env.NODE_ENV === "production"
+    ? env.REDIS_URL
+    : env.DEVELOPMENT_REDIS_URL;    
 const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
+  secure: env.NODE_ENV === "production",
   sameSite: "strict" as const,
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
 export default {
-  PORT: process.env.PORT || 3001,
+  PORT: env.PORT || 3001,
   MONGODB_URI,
-  BASE_URL: process.env.BASE_URL,
-  JWT_SECRET: process.env.JWT_SECRET,
-  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
-  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-  CLIENT_URL: process.env.CLIENT_URL || "http://localhost:5173",
-  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-  CLOUDINARY_SECRET: process.env.CLOUDINARY_API_SECRET,
-  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  REDIS_URL,
+  BASE_URL: env.BASE_URL,
+  JWT_SECRET: env.JWT_SECRET,
+  JWT_REFRESH_SECRET: env.JWT_REFRESH_SECRET,
+  GOOGLE_CLIENT_ID: env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: env.GOOGLE_CLIENT_SECRET,
+  GITHUB_CLIENT_ID: env.GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET: env.GITHUB_CLIENT_SECRET,
+  CLIENT_URL: env.CLIENT_URL || "http://localhost:5173",
+  CLOUDINARY_API_KEY: env.CLOUDINARY_API_KEY,
+  CLOUDINARY_SECRET: env.CLOUDINARY_API_SECRET,
+  CLOUDINARY_CLOUD_NAME: env.CLOUDINARY_CLOUD_NAME,
   cookieOptions,
 };
