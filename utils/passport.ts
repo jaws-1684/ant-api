82aa1d73eaa563d02156b3c80c452816f3f18f67
jaws <dis.131998@gmail.com>
2026-05-22 12:51:38 +0300

    add: passport configuration

---
+++ utils/passport.ts
@@ -0,0 +1,57 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as GithubStrategy } from "passport-github2";
+import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
+import type { VerifiedCallback } from "passport-jwt";
+import config from "./config.ts";
+import userService from "../services/userService.ts";
+import type { GithubProfile, GoogleProfile } from "../types.ts";
+
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.JWT_SECRET as string,
+    },
+    (payload: { userId: string }, done) => {
+      userService.findById(payload.userId).then((user) => {
+        if(user) return done(null, user);
+        return done(null, false);
+      }).catch(e => done(e, false));
+    },
+  ),
+);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: config.GOOGLE_CLIENT_ID as string,
+      clientSecret: config.GOOGLE_CLIENT_SECRET as string,
+      callbackURL: "/api/auth/google/callback",
+    },
+    (_accessToken, _refreshToken, profile, done) => {
+      userService
+      .addFromGoogleProfile(profile as GoogleProfile)
+      .then(user => done(null, user))
+      .catch(e => done(e, false));
+    },
+  ),
+);
+
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: config.GITHUB_CLIENT_ID as string,
+      clientSecret: config.GITHUB_CLIENT_SECRET as string,
+      callbackURL: "/api/auth/github/callback",
+    },
+    ( _accessToken: string, _refreshToken: string, profile: GithubProfile, done: VerifiedCallback,) => {
+       userService
+      .addFromGithubProfile(profile)
+      .then(user => done(null, user))
+      .catch(e => done(e, false));
+    },
+  ),
+);
+
+export default passport;
