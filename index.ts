5ba366837c406e383ad3c66654b679ceb7314958
jaws <dis.131998@gmail.com>
2026-05-25 12:26:55 +0300

    add: io socket and create http server

--- index.ts
+++ index.ts
@@ -2,12 +2,18 @@ import app from "./app.ts";
 import config from "./utils/config.ts";
 import logger from "./utils/logger.ts";
 import db from "./utils/db.ts";
+import io from "./utils/socket.ts";
+import http from "http";
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`);
-  process.on("SIGINT", () => {
-    db.disconnect()
-      .then(() => process.exit())
-      .catch((e: Error) => logger.error(e.message));
-  });
+const server = http.createServer(app);
+io.attach(server);
+
+await db.connect();
+server.listen(config.PORT, () => {
+ logger.info(`Server running on port ${config.PORT}`);
+});
+process.on("SIGINT", () => {
+  db.disconnect()
+    .then(() => process.exit())
+    .catch((e: Error) => logger.error(e.message));
 });
