341978b7c370a2af256bd82c2d65323e04cd0119
jaws <dis.131998@gmail.com>
2026-05-28 13:02:06 +0300

    refactor: to add broadcasting

--- services/chatService.ts
+++ services/chatService.ts
@@ -1,78 +1,76 @@
 import Chat from "../models/chatModel.ts";
 import Message from "../models/messageModel.ts";
-import type { ChatDocument, ChatDTO, MessageDocument, ChatServiceParams } from "../types.ts";
+import Notification from "../models/notificationModel.ts";
+import User from "../models/userModel.ts";
+import type { ChatDocument, ChatDTO, ChatServiceParams, ReturnedChat } from "../types.ts";
+import { withDTO } from "../utils/serializers.ts";
 interface ParticipantsTuple {
   participants: readonly [string, string];
 }
 type IsGroup = { group?: true | false };
+
+const _add = async ({
+  participants,
+}: ParticipantsTuple) => {
+  const chat = await findByParticipants({ participants });
+  if (chat) return chat;
+  return new Chat({ participants }).save();
+};
+const _del = async ({ id, userId }: ChatServiceParams) => {
+  return await Chat.findOneAndUpdate(
+    { _id: id, participants: userId },
+    { $push: { deletedFor: userId } },
+    { returnDocument: "after" },
+  ).orFail();
+};
+const _mark = async ({ id, userId }: ChatServiceParams) => {
+  const chat = await Chat.findOne({ _id: id }).orFail();
+  const notification = await Notification.findOne({ chatId: id, userId: { $ne: userId } });
+  if (!notification || notification.newMessages === 0) return chat;
+  await Notification.findOneAndUpdate(
+    { chatId: id, userId: { $ne: userId }, newMessages: { $ne: 0 }  },
+    { newMessages: 0 },
+    { upsert: true },
+  );
+  const marked = await Chat.findOneAndUpdate(
+    { _id: id, participants: userId },
+    { $set: { [`lastReadAt.${userId}`]: new Date() } },
+    { returnDocument: "after" },
+  );
+  return marked as ChatDocument;
+};
+const toDTO = async <T extends ChatDocument>(doc: T): Promise<ChatDTO> => {
+  const lastMessage = await Message.findOne({ _id: doc.lastMessage });
+  const notifications = await Notification.find({ chatId: doc._id });
+  const participants = await User.find({ '_id': { $in: doc.participants } });
+  const chat = doc.toJSON() as ReturnedChat & { id: string };  
+  return {
+    ...chat,
+    notifications: notifications.map(n => n.toJSON()),
+    participants: participants.map(p => ({ ...p.toJSON(), id: p._id.toString() })),
+    lastMessage: lastMessage?.toJSON()
+  };
+};
 const getChats = async (userId: string, { group }: IsGroup = { group: false }): Promise<ChatDTO[]> => {
   const chats = await Chat.find({
     participants: userId,
     deletedFor: { $nin: [userId] },
     isGroup: group,
     closed: false
-  }).populate("participants");
-
-  const chatsWithUnread = await Promise.all(
-    chats.map(async (chat: ChatDocument) => {
-      const lastRead = chat.lastReadAt?.get(userId) ?? new Date(0);
-      const [lastMessage, unread] = await getUnreadMessages({
-        id: chat.id,
-        userId,
-        lastRead,
-      });
-      return { ...chat.toJSON(), id: chat.id, unread, lastMessage };
-    }),
-  );
-
-  return chatsWithUnread.sort((a, b) => {
+  });
+  const dtos = await Promise.all(chats.map((c) => toDTO(c)));
+  return toSorted(dtos); 
+};
+//add broadcastings late and mutations if needed
+const addChat = withDTO(_add, toDTO);  
+const deleteChat = withDTO(_del, toDTO);
+const markAsRead = withDTO(_mark, toDTO);
+const toSorted = (chats: ChatDTO[]) => {
+  return chats.sort((a, b) => {
     const aTime = a.lastMessage?.createdAt?.getTime() ?? 0;
     const bTime = b.lastMessage?.createdAt?.getTime() ?? 0;
     return bTime - aTime;
   });
-};
-
-const addChat = async ({
-  participants,
-}: ParticipantsTuple): Promise<ChatDocument> => {
-  const chat = await findByParticipants({ participants });
-  if (chat) return chat;
-  return new Chat({ participants }).save();
-};
-const deleteChat = async ({ id, userId }: ChatServiceParams) => {
-  return Chat.findOneAndUpdate(
-    { _id: id, participants: userId },
-    { $push: { deletedFor: userId } },
-    { returnDocument: "after" },
-  ).orFail();
-};
-const markAsRead = async ({
-  id,
-  userId,
-}: ChatServiceParams): Promise<ChatDocument> => {
-  return Chat.findOneAndUpdate(
-    { _id: id, participants: userId },
-    { $set: { [`lastReadAt.${userId}`]: new Date() } },
-    { returnDocument: "after" },
-  )
-    .orFail()
-    .populate("participants");
-};
-const getUnreadMessages = async ({
-  id,
-  userId,
-  lastRead,
-}: ChatServiceParams & { lastRead: NativeDate }): Promise<
-  [MessageDocument | null, number]
-> => {
-  return Promise.all([
-    Message.findOne({ chatId: id }).sort({ createdAt: -1 }).limit(1),
-    Message.countDocuments({
-      chatId: id,
-      userId: { $ne: userId },
-      createdAt: { $gt: lastRead },
-    }),
-  ]);
 };
 const findById = async (id: string): Promise<null | ChatDocument> =>
   await Chat.findById(id);
@@ -98,4 +96,5 @@ export default {
   findOneUserChat,
   markAsRead,
   getChats,
+  toDTO
 };
