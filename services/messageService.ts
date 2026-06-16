3da668b02916eb3a8f5b1d3b369f20e486b991ec
jaws <dis.131998@gmail.com>
2026-06-16 13:03:35 +0300

    refactor: to use messageDTO transform

--- services/messageService.ts
+++ services/messageService.ts
@@ -1,8 +1,9 @@
-import type { NewMessage, MessageDTO, UpdateMessage, MessageDocument } from "../types.ts";
+import type { NewMessage, MessageDTO, UpdateMessage, MessageDocument, ReturnedMessage } from "../types.ts";
 import Message from "../models/messageModel.ts";
 import Notification from "../models/notificationModel.ts";
 import Chat from "../models/chatModel.ts";
 import socketService from "./socketService.ts";
+import { withMessageDTO } from "../utils/dto.ts";
 
 interface MessageServiceParams {
   id: string;
@@ -20,6 +21,39 @@ const LIMIT = 50;
 const OFFSET = 0;
 const PAGE = null;
 
+const _add = async (newMessage: NewMessage): Promise<MessageDocument> => {
+   await Chat.findOne({
+    _id: newMessage.chatId,
+    participants: newMessage.userId,
+  }).orFail();
+  const message = await new Message(newMessage).save();
+  await Notification.findOneAndUpdate(
+      { chatId: newMessage.chatId, userId: newMessage.userId },
+      { "$inc": { newMessages: 1 } },
+      { upsert: true },
+    );
+  return message;
+};
+const _update = async (
+  updateMessage: UpdateMessage,
+): Promise<MessageDocument> => {
+  const message = await Message.findOne({
+    _id: updateMessage.id,
+    userId: updateMessage.userId,
+  }).orFail();
+  message.isEdited = true;
+  message.content = updateMessage.content;
+  const updated = await message.save();
+  return updated;
+};
+const _delete = async ({ id, userId }: MessageServiceParams) => {
+  const deleted = await Message.findOneAndUpdate(
+    { _id: id, userId },
+    { softDeleted: true },
+    { returnDocument: "after" },
+  ).orFail();
+  return deleted;
+};
 const getMessages = async ({
   chatId,
   userId,
@@ -37,42 +71,18 @@ const getMessages = async ({
     .limit(limit);
   return messages.map(m => m.toJSON());
 };
-const addMessage = async (newMessage: NewMessage): Promise<MessageDocument> => {
-   await Chat.findOne({
-    _id: newMessage.chatId,
-    participants: newMessage.userId,
-  }).orFail();
-  const message = await new Message(newMessage).save();
-  await Notification.findOneAndUpdate(
-      { chatId: newMessage.chatId, userId: newMessage.userId },
-      { "$inc": { newMessages: 1 } },
-      { upsert: true },
-    );
-  await socketService.broadcastMessage("message:new", message);
-  return message;
-};
-const updateMessage = async (
-  updateMessage: UpdateMessage,
-): Promise<MessageDocument> => {
-  const message = await Message.findOne({
-    _id: updateMessage.id,
-    userId: updateMessage.userId,
-  }).orFail();
-  message.isEdited = true;
-  message.content = updateMessage.content;
-  const updated = await message.save();
-  await socketService.broadcastMessage("message:updated", updated);
-  return updated;
-};
-const deleteMessage = async ({ id, userId }: MessageServiceParams) => {
-  const deleted = await Message.findOneAndUpdate(
-    { _id: id, userId },
-    { softDeleted: true },
-    { returnDocument: "after" },
-  ).orFail();
-  await socketService.broadcastMessage("message:updated", deleted);
-  return deleted;
-};
+const addMessage = withMessageDTO(
+  _add, 
+  (message: MessageDTO) => socketService.broadcastMessage("message:new", message)
+);
+const updateMessage = withMessageDTO(
+  _update, 
+  (message: MessageDTO) => socketService.broadcastMessage("message:updated", message)
+);
+const deleteMessage = withMessageDTO(
+  _delete, 
+  (message: MessageDTO) => socketService.broadcastMessage("message:updated", message)
+);
 const findMessage = async ({ id, userId }: MessageServiceParams) => {
   return Message.findOne({ _id: id, userId });
 };
