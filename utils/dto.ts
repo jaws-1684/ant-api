75180f51db57b0e31a55773d27d4bcaa1853b752
jaws <dis.131998@gmail.com>
2026-06-23 12:29:26 +0300

    refactor: toChatDTO to use mongoose populate instead of separate db queries

--- utils/dto.ts
+++ utils/dto.ts
@@ -1,7 +1,7 @@
 import Message from "../models/messageModel.ts";
 import Notification from "../models/notificationModel.ts";
 import User from "../models/userModel.ts";
-import type { ChatDTO, ChatDocument, DTOType, DocumentType, MessageDTO, MessageDocument, UserDTO, UserDocument } from "../types.ts";
+import type { ChatDTO, ChatDocument, DTOType, DocumentType, MessageDTO, MessageDocument, NotificationDTO, NotificationDocument, UserDTO, UserDocument } from "../types.ts";
 import { NotFoundError } from "./errors.ts";
 
 export const initDTO = <Doc extends DocumentType, DTO extends DTOType>(
@@ -21,15 +21,28 @@ export const initDTO = <Doc extends DocumentType, DTO extends DTOType>(
   };
 };
 export const toChatDTO = async <T extends ChatDocument>(doc: T): Promise<ChatDTO> => {
-  const lastMessage = await Message.findOne({ _id: doc.lastMessage });
-  const notifications = await Notification.find({ chatId: doc._id });
-  const participants = await User.find({ '_id': { $in: doc.participants } });
-  const chat = doc.toJSON() as unknown as ChatDTO;
+  // const lastMessage = await Message.findOne({ _id: doc.lastMessage });
+  // const notifications = await Notification.find({ chatId: doc._id });
+  // const participants = await User.find({ '_id': { $in: doc.participants } });
+  const populated = await doc.populate([
+    { path: "lastMessage" },
+    { path: "participants" },
+    { path: "notifications"}
+  ]) as unknown as ChatDocument & {
+    lastMessage: MessageDocument | null;
+    participants: UserDocument[];
+    notifications: NotificationDocument[]
+  };
+
+  const lastMessage = populated.lastMessage;
+  const participants = populated.participants;
+  const notifications = populated.notifications;
+  const chat = populated.toJSON() as unknown as ChatDTO;
   return {
     ...chat,
-    notifications: notifications.map(n => n.toJSON()),
+    notifications: notifications.map(n => n.toJSON() as unknown as NotificationDTO),
     participants: participants.map(p => p.toJSON() as unknown as UserDTO),
-    lastMessage: lastMessage?.toJSON()
+    lastMessage: lastMessage?.toJSON() as unknown as MessageDTO
   };
 };
 export const toUserDTO = (user: UserDocument) => user.toJSON() as unknown as UserDTO;
